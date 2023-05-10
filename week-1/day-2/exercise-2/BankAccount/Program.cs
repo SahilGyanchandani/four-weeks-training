2e958b033a2092dedc686085ed12660b1660b3b4
SahilGyanchandani <gyanchandanisahil8@gmail.com>
2023-05-10 11:47:03 +0530

    bank1

--- week-1/day-2/exercise-2/BankAccount/Program.cs
+++ week-1/day-2/exercise-2/BankAccount/Program.cs
@@ -1,10 +1,62 @@
-﻿namespace BankAccount
+﻿using System;
+using System.Numerics;
+namespace BankAccount
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Create SavingsAccount and CheckingAccount objects and perform operations
+            SavingAccount SA = new SavingAccount(58009895612, 12000);
+            SA.deposit(100);
+            SA.withdrawal(50);
+            Checkingaccount CA = new Checkingaccount(59011276878, 8600);
+            CA.deposit(50);
+            CA.withdrawal(500);
+        }
+    }
+    abstract class BankAccount
+    {
+        public BigInteger AccNo;
+        public double balance;
+        public abstract void deposit(double amount);
+        public abstract void withdrawal(double amount);
+    }
+    class SavingAccount : BankAccount
+    {
+        double interestRate = 0;
+        public SavingAccount(BigInteger Accountnumber, double Balance)
+        {
+            this.AccNo = Accountnumber;
+            this.balance = Balance;
+        }
+        public override void deposit(double amount)
+        {
+            this.balance += amount;
+            Console.WriteLine($"{amount}Rs was deposited to your account number {this.AccNo}, your current balance is {this.balance}");
+        }
+        public override void withdrawal(double amount)
+        {
+            this.balance -= amount;
+            Console.WriteLine($"{amount}Rs was withdrew from your account number {this.AccNo}, your current balance is {this.balance}");
+        }
+    }
+    class Checkingaccount : BankAccount
+    {
+        double overdraftlimit = 0;
+        public Checkingaccount(BigInteger Accountnumber, double Balance)
+        {
+            this.AccNo = Accountnumber;
+            this.balance = Balance;
+        }
+        public override void deposit(double amount)
+        {
+            this.balance += amount;
+            Console.WriteLine($"{amount}Rs was deposited to your account number {this.AccNo}, your current balance is {this.balance}");
+        }
+        public override void withdrawal(double amount)
+        {
+            this.balance -= amount;
+            Console.WriteLine($"{amount}Rs was withdrew from your account number {this.AccNo}, your current balance is {this.balance}");
         }
     }
 }
