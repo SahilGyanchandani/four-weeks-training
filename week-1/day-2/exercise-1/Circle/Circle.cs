ed4477a07be4d010d8b4e6fcc2f6927c696bac9b
SahilGyanchandani <gyanchandanisahil8@gmail.com>
2023-05-10 11:45:29 +0530

    circle1

--- week-1/day-2/exercise-1/Circle/Circle.cs
+++ week-1/day-2/exercise-1/Circle/Circle.cs
@@ -3,11 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
-namespace Circle
+namespace AreaOfCircle
 {
-    internal class Circle
+    internal class AOC
     {
-        // Implement the Circle class here
+        double Radius;
+
+        static void Main(string[] args)
+        {
+            double radius = 0;
+            Console.WriteLine("Enter the value of radius:");
+            radius = Convert.ToDouble(Console.ReadLine());
+
+
+            AOC circle = new AOC(radius);
+
+            Console.WriteLine("Area Of Circle: " + circle.getarea());
+            Console.WriteLine("Aree of circumference: " + circle.circumference());
+
+        }
+        public AOC(double radius)
+        {
+            this.Radius = radius;
+        }
+        public double getarea()
+        {
+            double pi = 0;
+            double area = 0;
+
+            pi = 3.14;
+            area = (pi) * this.Radius * this.Radius;
+            return area;
+        }
+
+        public double circumference()
+        {
+            double circumference = 0;
+            double pi = 0;
+
+            pi = 3.14;
+            circumference = 2 * pi * this.Radius;
+            return circumference;
+
+        }
     }
 }
