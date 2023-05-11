8c568569bdb247f967a28dcaf2c2ab215d969ba7
Ashishdesk1 <132877667+Ashishdesk1@users.noreply.github.com>
2023-05-11 12:56:29 +0530

    Day-4 Exercise1

--- week-1/day-4/exercise-1/StackApp/Program.cs
+++ week-1/day-4/exercise-1/StackApp/Program.cs
@@ -1,9 +1,72 @@
-﻿namespace StackApp
+﻿using System;
+using System.Collections.Generic;
+namespace StackApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Stack<int> stack= new Stack<int>();
+            Stack<string> stack2 = new Stack<String>();
+
+            Console.WriteLine("Enter the  Integer Value of Stack");
+            stack.Push(Convert.ToInt32(Console.ReadLine()));
+            stack.Push(Convert.ToInt32(Console.ReadLine()));
+            stack.Push(Convert.ToInt32(Console.ReadLine()));
+            stack.Push(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Enter the String Value:");
+            stack2.Push(Console.ReadLine());
+            stack2.Push(Console.ReadLine());
+            Console.WriteLine("--------------------------------------------------------------------");
+
+            Console.WriteLine("Elements in Stacks after Push in Integer:");
+            foreach(var item in stack)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("Elements in Stacks after Push in Integer:");
+            foreach (var item in stack2)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.WriteLine($"No of Element in Integer Stack:{stack.Count}");
+            Console.WriteLine($"No of Element in String Stack:{stack2.Count}");
+            Console.WriteLine("--------------------------------------------------------------------");
+
+
+
+
+            stack.Pop();
+            stack.Pop();
+            stack2.Pop();
+            
+           /* stack2.Pop()*/;
+
+            Console.WriteLine("ELment After Pop:");
+            foreach (var item in stack )
+            {
+                Console.WriteLine(item);
+
+            }
+            foreach(var item in stack2)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine("--------------------------------------------------------------------");
+
+            }
+            if (stack.Count <= 0)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            else
+            {
+                Console.WriteLine("Stack is Full");
+            }
+            Console.WriteLine("--------------------------------------------------------------------");
+
+
+
             //ICustomStack<int> intStack = new CustomStack<int>();
             //intStack.Push(1);
             //intStack.Push(2);
