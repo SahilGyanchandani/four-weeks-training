d88105e8e98eba854d00caedd36c7db8154f8b1e
SahilGyanchandani <gyanchandanisahil8@gmail.com>
2023-05-12 16:46:20 +0530

    FileLoggerDisposableApp

--- week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
+++ week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
@@ -1,29 +1,37 @@
-﻿namespace FileLoggerDisposableApp
+﻿using System;
+namespace FileLoggerDisposableApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            using (var logger = new FileLogger("Sahil Gyanchandani.txt"))
+            {
+                logger.Log("I am Sahil...!");
+                // some code here
+                logger.Log("I'm a unemployeed.....!!");
+                logger.Dispose();
+               
+            }
             // Use FileLogger and dispose of it properly
         }
     }
-
     class FileLogger : IDisposable
     {
         private StreamWriter _writer;
-
         public FileLogger(string filePath)
         {
+            _writer = new StreamWriter(filePath);
             // Initialize StreamWriter instance
         }
-
         public void Dispose()
         {
+            _writer.Dispose();
             // Implement IDisposable pattern
         }
-
         public void Log(string message)
         {
+            _writer?.WriteLine(message);
             // Write message to the file
         }
     }
