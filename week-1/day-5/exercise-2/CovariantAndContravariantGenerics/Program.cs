d739fcdc1d7224140d52fd072e49e8d21c047a39
SahilGyanchandani <gyanchandanisahil8@gmail.com>
2023-05-12 17:14:26 +0530

    Day 4 Ex-2

--- week-1/day-5/exercise-2/CovariantAndContravariantGenerics/Program.cs
+++ week-1/day-5/exercise-2/CovariantAndContravariantGenerics/Program.cs
@@ -1,32 +1,59 @@
-﻿namespace CovariantAndContravariantGenerics
+﻿using System;
+using System.IO;
+// Create an interface called "IProcessor<in TInput, out TResult>" with a single method "TResult Process(TInput input)".
+public interface IProcessor<in TInput, out TResult>
 {
-    interface IProcessor<in TInput, out TResult>
+    TResult Process(TInput input);
+}
+// Create a class called "StringToIntProcessor" that implements "IProcessor<string, int>" and returns the length of the input string.
+public class StringToIntProcessor : IProcessor<string, int>
+{
+    public int Process(string input)
     {
-        TResult Process(TInput input);
-    }
-
-    class StringToIntProcessor : IProcessor<string, int>
-    {
-        // Implement Process method
-        public int Process(string input)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    class DoubleToStringProcessor : IProcessor<double, string>
-    {
-        // Implement Process method
-        public string Process(double input)
-        {
-            throw new NotImplementedException();
-        }
-    }
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // Demonstrate covariance and contravariance with IProcessor interface
-        }
+        return input.Length;
     }
 }
+// Create a class called "DoubleToStringProcessor" that implements "IProcessor<double, string>" and returns the string representation of the input double
+public class DoubleToStringProcessor : IProcessor<double, string>
+{
+    public string Process(double input)
+    {
+        return input.ToString();
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        StringToIntProcessor stringToIntProcessor = new StringToIntProcessor();
+        int output1 = stringToIntProcessor.Process("Sahil");
+        Console.WriteLine(output1);
+        DoubleToStringProcessor doubleToStringProcessor = new DoubleToStringProcessor();
+        string output2 = doubleToStringProcessor.Process(3.14);
+        Console.WriteLine(output2);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
