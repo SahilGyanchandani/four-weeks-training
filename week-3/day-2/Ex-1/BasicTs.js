0626066214fa2f5b15bba97a473f5de6d3f6e538
Ujjaval-99 <ujjavalmistry99@gmail.com>
2023-05-24 17:51:44 +0530

    Ex-1

---
+++ week-3/day-2/Ex-1/BasicTs.js
@@ -0,0 +1,7 @@
+function greet(name) {
+    return "Hello, ".concat(name, "!");
+}
+// Test the greet function with different inputs
+console.log(greet("Alice"));
+console.log(greet("Bob"));
+console.log(greet("Charlie"));
---
+++ week-3/day-2/Ex-1/BasicTs.ts
@@ -0,0 +1,7 @@
+function greet(name: string): string {
+  return `Hello, ${name}!`;
+}
+// Test the greet function with different inputs
+console.log(greet("Alice"));
+console.log(greet("Bob"));
+console.log(greet("Charlie"));
